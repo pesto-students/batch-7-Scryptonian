0a614f15d27471eda1fdb0d3d31ded4efaa576d0
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-29 05:54:18 +0530

    chore: Add babel and nodemon to use ES6 features
    Also add dummy GET /users and GET /boards routes.

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import users from './routes/users';
+import boards from './routes/boards';
+
+import { PORT } from './configs/config'
+
+const app = express();
+
+app.use('/users/', users);
+app.use('/boards/', boards);
+
+app.get('/', (req, res) => {
+    res.send('Hey!');
+});
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
---
+++ server/routes/boards.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hey from GET boards/');
+});
+
+export default router;
