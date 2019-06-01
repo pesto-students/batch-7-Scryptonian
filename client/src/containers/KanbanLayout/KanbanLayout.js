9175aef65d4e8dc36fa7378a1432b559d931db03
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-01 07:04:49 +0530

    feat: Created KanbanLayout and Lifecycle React components

---
+++ client/src/containers/KanbanLayout/KanbanLayout.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import LifecyclesContainer from '../LifecyclesContainer/LifecyclesContainer';
+
+const KanbanLayout = props => <LifecyclesContainer {...props} />;
+
+export default KanbanLayout;
