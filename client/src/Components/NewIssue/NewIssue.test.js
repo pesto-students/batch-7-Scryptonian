39d777ce2bd697a692a5bbc79112930a5f409933
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-04 02:32:40 +0530

    feat: Create component to add New Issue in Kanban layout

---
+++ client/src/Components/NewIssue/NewIssue.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import NewIssue from './NewIssue.js';
+import { Card, Button, EditableText } from '@blueprintjs/core';
+
+describe('<NewIssue />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NewIssue />);
+  });
+
+  it('should render a <Card /> component', () => {
+    expect(wrapper.find(Card)).toHaveLength(1);
+  });
+
+  it('should render a <Button /> component to Add issue', () => {
+    expect(wrapper.find(Button)).toHaveLength(1);
+    expect(wrapper.find(Button).props().text).toBe('Add issue');
+  });
+
+  it('should render an <EditableText /> component with correct props', () => {
+    expect(wrapper.find(EditableText)).toHaveLength(1);
+    expect(wrapper.find(EditableText).props().confirmOnEnter).toBe(false);
+    expect(wrapper.find(EditableText).props().placeholder).toBe('New Issue...');
+    expect(wrapper.find(EditableText).props().multiline).toBe(true);
+    expect(wrapper.find(EditableText).props().minLines).toBe(2);
+  });
+});
