1f0a7fc2898b5cb8b39414f5982dea79be683e63
Pratiyush <pkpratiyush@gmail.com>
2019-06-06 06:04:10 +0530

    feat: Create a Board Component

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,18 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 import SignUp from './containers/SignUp/SignUp';
-import { shallow } from "enzyme";
+import { shallow } from 'enzyme';
 
-describe("App component", () => {
-  it("Test Render component using shallow", () => {
+describe('App component', () => {
+  it('should render a SignUp Component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(SignUp)).toHaveLength(1);
   });
-});
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  it('should render without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
---
+++ client/src/components/Board/BoardBox.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import BoardBox from './BoardBox';
+import { Card } from '@blueprintjs/core';
+
+describe('<BoardBox />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<BoardBox />);
+  });
+  it('Should have a Card element', () => {
+    expect(wrapper.find(Card)).toHaveLength(1);
+  });
+
+  it('Should have a <p> element', () => {
+    expect(wrapper.find('p')).toHaveLength(1);
+  });
+
+  it('Should render Create New element', () => {
+    expect(wrapper.find('p').text()).toEqual('Create New');
+  });
+});
