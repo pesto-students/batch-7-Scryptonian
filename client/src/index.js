b4fe062bd21b56062651198b853adec816663eb3
Rajat <rajat.srivastava.20893@gmail.com>
2019-06-06 23:55:31 +0530

    chore: Setup redux and redux devtools support

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './reducers/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 serviceWorker.unregister();
