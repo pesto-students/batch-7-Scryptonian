e5504f90f1ba04f2d973c6eb3e25ecf921ae495e
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-08 04:33:04 +0530

    refactor: Refactor index.js and add constants for HTTP status code

--- server/configs/config.js
+++ server/configs/config.js
@@ -1,1 +1,2 @@
 export const PORT = process.env.PORT || 8000;
+export const MONGO_CONNECTION_STRING = 'mongodb+srv://admin:FRsojwU0AjxQyWhS@scryptonian-ghufh.mongodb.net/test?retryWrites=true&w=majority';
---
+++ server/configs/httpStatusCodes.js
@@ -0,0 +1,5 @@
+export const OK = 200;
+export const UNAUTHORIZED = 401;
+export const FORBIDDEN = 403;
+export const NOT_FOUND = 404;
+export const INTERNAL_SERVER_ERROR = 500;
