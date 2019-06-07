20fe53629ff226a2c6e4e7f1a938a0e1ce02dffe
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-08 04:33:04 +0530

    refactor: Refactor index.js and add constants for HTTP status code

--- server/index.js
+++ server/index.js
@@ -1,16 +1,29 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
 import users from './routes/users';
 import boards from './routes/boards';
 
-import { PORT } from './configs/config';
+import { PORT, MONGO_CONNECTION_STRING } from './configs/config';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/users/', users);
 app.use('/boards/', boards);
 
 app.get('/', (req, res) => {
   res.send('Hey!');
+});
+
+mongoose.connect(MONGO_CONNECTION_STRING, { useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log('MongoDB connection is established.');
 });
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
