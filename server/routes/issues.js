81d61677175ead58b5b92aecd2f9b9926548f357
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-09 05:34:29 +0530

    feat: Add GET /issue/id routes

--- server/routes/issues.js
+++ server/routes/issues.js
@@ -1,11 +1,37 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from '../configs/httpStatusCodes';
+import {
+  BAD_REQUEST, INTERNAL_SERVER_ERROR, OK, NOT_FOUND,
+} from '../configs/httpStatusCodes';
 import Lifecycle from '../models/lifecycle';
 import Issue from '../models/issue';
+import User from '../models/user';
 
 const router = express.Router();
+
+router.get('/:issueid', async (req, res) => {
+  const { issueid } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(issueid)) {
+    return res.status(BAD_REQUEST).send('Invalid issueID');
+  }
+
+  let issue;
+  try {
+    issue = await Issue.findById(issueid)
+      .populate('createdBy')
+      .exec();
+  } catch (e) {
+    return res.status(INTERNAL_SERVER_ERROR).send(e.message);
+  }
+
+  if (issue === null) {
+    return res.status(NOT_FOUND).send(`Issue with id ${issueid} is not present.`);
+  }
+
+  return res.status(OK).send(issue);
+});
 
 router.post('/', async (req, res) => {
   const { lifecycleid, issue, createdBy } = req.body;
