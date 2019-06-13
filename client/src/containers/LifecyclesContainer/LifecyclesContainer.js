5a94062290ca6062b3f4377a551fc8fb1db0a591
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-14 01:13:44 +0530

    feat: Add New Issue functionality

--- client/src/containers/LifecyclesContainer/LifecyclesContainer.js
+++ client/src/containers/LifecyclesContainer/LifecyclesContainer.js
@@ -5,7 +5,14 @@ import classes from './LifecyclesContainer.module.css';
 const LifecyclesContainer = props => (
   <div className={classes.Lifecycles}>
     {props.lifecycles
-      ? props.lifecycles.map((lifecycle, index) => <Lifecycle name={lifecycle.name} key={lifecycle._id} issues={lifecycle.issues} />)
+      ? props.lifecycles.map((lifecycle, index) => (
+          <Lifecycle
+            name={lifecycle.name}
+            key={lifecycle._id}
+            issues={lifecycle.issues}
+            lifecycleid={lifecycle._id}
+          />
+        ))
       : null}
   </div>
 );
