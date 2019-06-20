1c7176df1c77f385f78e0e4113d8168d707de113
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-20 09:28:54 +0530

    add proxy

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,10 @@
 export const GoogleLoginUrl = process.env.REACT_APP_SERVER_LOGIN_URL;
-export const BASE_URL = process.env.BASE_URL || 'http://localhost:8000';
+export const BASE_URL = process.env.REACT_APP_BASE_URL || '' || 'http://localhost:8000';
 export const LogoutUrl = process.env.REACT_APP_SERVER_LOGOUT_URL;
 
 // Member Roles
 export const ROLE = {
   USER: 'USER',
   ADMIN: 'ADMIN',
-  SUPERADMIN: 'SUPERADMIN'
-}
+  SUPERADMIN: 'SUPERADMIN',
+};
