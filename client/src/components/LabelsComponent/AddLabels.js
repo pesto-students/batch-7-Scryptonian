bab701a74cf3ad279e3fa044f234fb17c0fa08c3
Pratiyush <pkpratiyush@gmail.com>
2019-06-21 12:06:16 +0530

    User Role dropdown and Twitter color picker

--- client/src/components/LabelsComponent/AddLabels.js
+++ client/src/components/LabelsComponent/AddLabels.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './AddLabels.css';
-import { Popover, Position, Classes, Button, Menu } from '@blueprintjs/core';
-import { BlockPicker } from 'react-color';
+import {
+  Popover,
+  Position,
+  Classes,
+  Button,
+  Menu,
+  InputGroup
+} from '@blueprintjs/core';
+import { TwitterPicker } from 'react-color';
 import axios from 'axios';
 import { BASE_URL } from '../../config';
 import { connect } from 'react-redux';
@@ -24,8 +31,8 @@ class AddLabels extends React.Component {
       data: {
         color: this.state.color,
         labelName: this.state.name,
-        boardId: this.props.currentBoardId,
-      },
+        boardId: this.props.currentBoardId
+      }
     })
       .then(() => successToast('New label created'))
       .catch(e => {
@@ -35,39 +42,46 @@ class AddLabels extends React.Component {
 
   render() {
     return (
-      <>
-        <Popover
-          content={
-            <Menu className={Classes.ELEVATION_ONE}>
-              <div className="add-name">
-                <label>
-                  Name:
-                  <input type="text" value={this.state.name} onChange={this.handleNameChange} />
-                </label>
-              </div>
-              <div className="add-color">
-                <label>
-                  Color:
-                  <BlockPicker color={this.state.color} onChangeComplete={this.handleColorChange} />
-                </label>
-              </div>
-              <div className={Classes.POPOVER_DISMISS}>
-                <button onClick={this.handleSubmit}>Add new Label</button>
-              </div>
-            </Menu>
-          }
-          position={Position.BOTTOM_LEFT}
-        >
-          <Button rightIcon="arrow-down" text="Add Labels" />
-        </Popover>
-      </>
+      <Popover
+        content={
+          <Menu className={Classes.ELEVATION_ONE} style={{ display: 'block' }}>
+            <div className="add-name">
+              <label>
+                Name:
+                <InputGroup
+                  type="text"
+                  value={this.state.name}
+                  onChange={this.handleNameChange}
+                />
+              </label>
+            </div>
+            <div className="add-color">
+              <label>
+                Color:
+                <div style={{ margin: '14px' }}>
+                  <TwitterPicker
+                    color={this.state.color}
+                    onChangeComplete={this.handleColorChange}
+                  />
+                </div>
+              </label>
+            </div>
+            <div className={Classes.POPOVER_DISMISS}>
+              <button onClick={this.handleSubmit}>Add new Label</button>
+            </div>
+          </Menu>
+        }
+        position={Position.BOTTOM_LEFT}
+      >
+        <Button rightIcon="arrow-down" text="Add Labels" />
+      </Popover>
     );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    currentBoardId: state.currentBoardId,
+    currentBoardId: state.currentBoardId
   };
 };
 
