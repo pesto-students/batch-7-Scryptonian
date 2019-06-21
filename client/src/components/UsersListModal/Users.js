68056a16276f24856b5357a7e24f6e7549171de1
Pratiyush <pkpratiyush@gmail.com>
2019-06-21 09:34:52 +0530

    Add Dropdown to UserRole

--- client/src/components/UsersListModal/Users.js
+++ client/src/components/UsersListModal/Users.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Button } from '@blueprintjs/core';
+import {
+  Button,
+  Popover,
+  Menu,
+  MenuItem,
+  MenuDivider,
+  Position
+} from '@blueprintjs/core';
 import './Users.css';
+import { connect } from 'react-redux';
 
 class Users extends React.Component {
   render() {
+    console.log(this.props.roleInCurrentBoard);
     return (
       <div className="row">
         <div className="column">
           <h3>Users</h3>
-          {this.props.members.map((user) => (
+          {this.props.members.map(user => (
             <div key={user._id} className="content">
               {user.membername}
             </div>
@@ -16,11 +25,36 @@ class Users extends React.Component {
         </div>
         <div className="column">
           <h3>Role</h3>
-          {this.props.members.map((user) => (
+          {this.props.members.map(user => (
             <div key={user._id} className="content">
-              <Button intent="success" small="true">
-                {user.role}
-              </Button>
+              {this.props.roleInCurrentBoard === 'SUPERADMIN' ? (
+                <Popover
+                  content={
+                    <Menu style={{ display: 'block' }}>
+                      <MenuItem
+                        text=" Make SuperAdmin"
+                        icon="edit"
+                        intent="primary"
+                      />
+                      <MenuDivider />
+                      <MenuItem
+                        text="Make Admin"
+                        icon="edit"
+                        intent="primary"
+                      />
+                      <MenuDivider />
+                      <MenuItem text="Make User" icon="edit" intent="primary" />
+                    </Menu>
+                  }
+                  position={Position.BOTTOM_RIGHT}
+                >
+                  <Button small="true" rightIcon="caret-down">
+                    {user.role}
+                  </Button>
+                </Popover>
+              ) : (
+                <Button small="true">{user.role}</Button>
+              )}
             </div>
           ))}
         </div>
@@ -29,4 +63,10 @@ class Users extends React.Component {
   }
 }
 
-export default Users;
+const mapStateToProps = state => {
+  return {
+    roleInCurrentBoard: state.roleInCurrentBoard
+  };
+};
+
+export default connect(mapStateToProps)(Users);
